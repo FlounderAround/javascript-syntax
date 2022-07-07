ecddffeb19d14ce38f4e43297f4d149ce954bd82
221B0825 <eunseoyu0825@gmail.com>
2022-07-07 17:31:24 +0900

    ch04 commit

--- ch04.js
+++ ch04.js
@@ -15,3 +15,33 @@ switch(ID){
     default:
         alert('1~4 사이의 정수를 입력하여 주세요')
 }
+
+
+//요일은 0~6사이의 인덱스 번호로 반환
+var day = new Date().getDay();
+
+alert('오늘 : '+day);
+
+switch(day){
+    case 0:
+        day = '일요일';
+        break;
+    case 1:
+        day = '월요일';
+        break;
+    case 2:
+        day = '화요일';
+        break;
+    case 3:
+        day = '수요일';
+        break;
+    case 4:
+        day = '목요일';
+        break;
+    case 5:
+        day = '금요일';
+        break;
+    case 6:
+        day = '토요일';
+        break;
+}
